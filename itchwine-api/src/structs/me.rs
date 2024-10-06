93ddb7d583d22385680d1f170408f3440adec9e5
verifiedfemboy <maciekplaygamesyt@gmail.com>
2024-10-06 22:27:57 +0200

    saving tokens

--- itchwine-api/src/structs/me.rs
+++ itchwine-api/src/structs/me.rs
@@ -2,10 +2,12 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Me {
-    pub id: i32,
+    pub id: i64,
     pub username: String,
+    pub display_name: Option<String>,
     pub developer: bool,
     pub press_user: bool,
     pub url: String,
+    pub cover_url: Option<String>,
     pub gamer: bool,
 }
--- itchwine-tui/src/main.rs
+++ itchwine-tui/src/main.rs
@@ -1,6 +1,9 @@
 use std::{thread::sleep, time::Duration};
 
 use itchwine_api::api::Api;
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
 
 #[tokio::main]
 async fn main() {
@@ -9,8 +12,24 @@ async fn main() {
         redirect_uri: "http://localhost:8080/callback".to_string(),
     };
 
-    config.open_browser();
-    let api = Api::new().await;
+    let token_path = "/run/media/verifiedfemboy/Disk/programming/Rust/ItchWine/itchwine-tui/token.txt";
+
+    let mut api = Api::new();
+    if let Ok(mut file) = File::open(token_path) {
+        let mut token = String::new();
+        file.read_to_string(&mut token).unwrap();
+        println!("Token found: {}", token);
+        api.token = token;
+    } else {
+        config.open_browser();
+        api = api.get_token().await;
+        let mut file = File::create(token_path).unwrap();
+        let placeholder_token = api.token.clone();
+        file.write_all(placeholder_token.as_bytes()).unwrap();
+        println!("Token file created with placeholder token.");
+    }
+
+
     let me = api.get_me().await.unwrap();
     println!("Me: {:?}", me);
     sleep(Duration::from_secs(3));
